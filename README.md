8cf23787095301f2644487750c4a4db39a08b60e
green-ogre <109698029+void-scape@users.noreply.github.com>
2026-02-21 20:49:18 -0700

    bgr

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Example
 
 ```rust
-let srgb = Srgb::rgb(r, g, b);
+let srgb = Srgb::from_rgb(r, g, b);
 let mut linear = srgb.to_linear();
 linear *= 0.5;
 let srgb = linear.to_srgb();
@@ -22,13 +22,3 @@ performance.
 The `Srgb` to `LinearRgb` conversion is performed with a loss-less, 256 byte lookup table 
 index for each color component (red, green, and blue). Likewise, the `LinearRgb` to `Srgb`
 conversion uses a lossy 3KB lookup table.
-
-Round trip conversions from `Srgb` to `LinearRgb` and back to `Srgb` are garaunteed
-to have a maximum error of 1 for each color component.
-
-## Performance
-
-Conversions with `Hsv` are not optimized.
-
-Converting between `Srgb` and `LinearRgb` is an order of magnitude faster than alternatives 
-that directly compute gamma correction.
--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 //!
 //! ## Motivation
 //!
-//! I wrote a [software rasterizer called rast](https://github.com/void-scape/blaze)
+//! I wrote a [software rasterizer called rast](https://github.com/void-scape/rast)
 //! that needed to convert between the sRGB and Linear RGB color spaces without crippling
 //! performance.
 //!
@@ -24,16 +24,6 @@
 //! The `Srgb` to `LinearRgb` conversion is performed with a loss-less, 256 byte lookup table
 //! index for each color component (red, green, and blue). Likewise, the `LinearRgb` to `Srgb`
 //! conversion uses a lossy 3KB lookup table.
-//!
-//! Round trip conversions from `Srgb` to `LinearRgb` and back to `Srgb` are garaunteed
-//! to have a maximum error of 1 for each color component.
-//!
-//! ## Performance
-//!
-//! Conversions with `Hsv` are not optimized.
-//!
-//! Converting between `Srgb` and `LinearRgb` is an order of magnitude faster than alternatives
-//! that directly compute gamma correction.
 
 #![no_std]
 
